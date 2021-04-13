d9e320f1ab28ade8525eceeebb114015db67247e
hiroykim <kimjunwon@gmail.com>
2021-04-13 22:32:59 +0900

    jwkim

---
+++ gpu_mem2.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+tf.debugging.set_log_device_placement(True)
+
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+print(gpus)
+if gpus:
+  # 텐서플로가 첫 번째 GPU만 사용하도록 제한
+  try:
+    tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+  except RuntimeError as e:
+    # 프로그램 시작시에 접근 가능한 장치가 설정되어야만 합니다
+    print(e)
