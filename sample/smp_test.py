ae6c75c83794083e613a7cddee583eb525317f36
hiroykim <kimjunwon@gmail.com>
2021-04-18 17:01:17 +0900

    jwkim

--- sample/smp_test.py
+++ sample/smp_test.py
@@ -1,20 +1,21 @@
-
 def map_test():
-    dt_smpl = {"a" :1, "b" :2, "c" :3}
-    lt_smpl = ["a" ,"b" ,"c"]
-    lt_smpl2 = [1 ,2 ,3]
+    dt_smpl = {"a": 1, "b": 2, "c": 3}
+    lt_smpl = ["a", "b", "c"]
+    lt_smpl2 = [1, 2, 3]
 
     lt_rst = list(map(lambda i: dt_smpl[i], lt_smpl))
     print(lt_rst)
 
+
 def dict_reverse_test(dt_org):
-    dt_new =dict()
+    dt_new = dict()
 
     for k, v in dt_org.items():
         print(k, v)
-        dt_new[v] =k
+        dt_new[v] = k
 
     print(dt_new)
+
 
 def dict_reverse_test2(dt_org):
     dt_new = dict([(v, k) for k, v in dt_org.items()])
@@ -22,6 +23,7 @@ def dict_reverse_test2(dt_org):
     lt_smpl2 = [1, 2, 3, 6]
     lt_new = [dt_new.get(i, "?") for i in lt_smpl2]
     print(lt_new)
+
 
 if __name__ == "__main__":
     # map_test()
