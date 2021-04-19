124cc5212bc17fa432bd54857b47acd704b6a5c8
hiroykim <kimjunwon@gmail.com>
2021-04-20 00:08:57 +0900

    jwkim

---
+++ README.txt
@@ -0,0 +1,2 @@
+#  'str' object has no attribute 'decode'
+  -> pip install h5py==2.10.0
--- dssrc/python/web/kmv_model.py
+++ dssrc/python/web/kmv_model.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import pandas as pd
 import calendar
@@ -206,4 +208,24 @@ def kmv_model():
 
 
 if __name__ == "__main__":
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    '''
+    if gpus:
+        try:
+            tf.config.experimental.set_memory_growth(gpus[0], True)
+        except RuntimeError as e:
+            # 프로그램 시작시에 메모리 증가가 설정되어야만 합니다
+            print(e)
+    '''
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+
     kmv_model()
