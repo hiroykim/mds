77d12efb65d68e0de04a42d028e059d548a32290
hiroykim <kimjunwon@gmail.com>
2021-04-22 21:59:19 +0900

    jwkim

--- dssrc/python/start_kmv_web_g1_8111.sh
+++ dssrc/python/start_kmv_web_g1_8111.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec gunicorn -w 10 --timeout=10 -b 0.0.0.0:8111 start_kmv_api_g1:app
+exec gunicorn -w 20 --timeout=30 -b 0.0.0.0:8111 start_kmv_api_g1:app
--- dssrc/python/start_kmv_web_g2_8122.sh
+++ dssrc/python/start_kmv_web_g2_8122.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec gunicorn -w 10 --timeout=3 -b 0.0.0.0:8122 start_kmv_api_g2:app
+exec gunicorn -w 20 --timeout=30 -b 0.0.0.0:8122 start_kmv_api_g2:app
