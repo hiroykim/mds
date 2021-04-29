dcdb6cc32deda78c713ed8f768949577317ce909
hiroykim <kimjunwon@gmail.com>
2021-04-29 17:13:03 +0900

    jwkim

--- dssrc/python/client_call.py
+++ dssrc/python/client_call.py
@@ -7,11 +7,6 @@ __date__ = "creation: 2021-03-06, modification: 0000-00-00"
 #################
 #import
 #################
-from flask import Flask, request
-from flask_restful import Api, Resource
-from flask_cors import CORS
-from web import kmv_api
-import os
 import requests
 import json
 
